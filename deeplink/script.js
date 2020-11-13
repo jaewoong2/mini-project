ff29cc01f72c3f40f8d465ce9308471f14106234
unknown <jwisgenius@naver.com>
2020-11-13 23:12:49 +0900

    deep link

--- deeplink/script.js
+++ deeplink/script.js
@@ -8,9 +8,9 @@
         // 인텐트 주소
     } else if(userInfo.indexOf('iphone') > -1 || userInfo.indexOf('ipad') > -1 || userInfo.indexOf('ipod') > -1) {
         setTimeout(() => {
-            window.location.href = "https://apps.apple.com/kr/app/%EA%B7%B8%EB%9D%BC%EC%9A%B4%EB%93%9C/id1483838254"
+            window.location.href = "https://apps.apple.com/kr/app/그라운드/id1483838254"
         }, 5000);
-        window.location.href = "https://apps.apple.com/kr/app/%EA%B7%B8%EB%9D%BC%EC%9A%B4%EB%93%9C/id1483838254";
+        window.location.href = "https://apps.apple.com/kr/app/그라운드/id1483838254";
         
     } else {
         console.log('아무것도 아님')
