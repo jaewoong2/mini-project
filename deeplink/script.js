ae6655d4f01aa05c7bef246433fdb0e4adfad2d5
unknown <jwisgenius@naver.com>
2020-11-13 13:05:07 +0900

    deep

--- deeplink/script.js
+++ deeplink/script.js
@@ -1,7 +1,7 @@
 (() => {
     const userInfo = navigator.userAgent.toLowerCase();
     if(userInfo.indexOf('android') > -1) {
-        window.location.href = "intent://instagram.com/#Intent;package=com"
+        window.location.href = "https://play.google.com/store/apps/details?id=net.daum.android.map"
     } else if(userInfo.indexOf('iphone') > -1 || userInfo.indexOf('ipad') > -1 || userInfo.indexOf('ipod') > -1) {
         window.location.href = "https://itunes.apple.com/kr/app/instagram/id389801252?mt=8";
     } else {
