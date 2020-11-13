11c0f98eb8bf934e3709e6b60bb6986f0776bb32
unknown <jwisgenius@naver.com>
2020-11-13 13:00:56 +0900

    deep link

--- deeplink/script.js
+++ deeplink/script.js
@@ -1,8 +1,8 @@
 (() => {
     const userInfo = navigator.userAgent.toLowerCase();
-    if(userInfo.indexOf('android')) {
+    if(userInfo.indexOf('android') > -1) {
         window.location.href = "intent://instagram.com/#Intent;package=com"
-    } else if(userInfo.indexOf('iphone') || userInfo.indexOf('ipad') || userInfo.indexOf('ipod')) {
+    } else if(userInfo.indexOf('iphone') > -1 || userInfo.indexOf('ipad') > -1 || userInfo.indexOf('ipod') > -1) {
         window.location.href = "https://itunes.apple.com/kr/app/instagram/id389801252?mt=8";
     } else {
         console.log('아무것도 아님')
