aaa7049b8d3b974c2a63ff622fb887d4ad88fe18
unknown <jwisgenius@naver.com>
2020-11-13 23:11:09 +0900

    deep link

--- deeplink/script.js
+++ deeplink/script.js
@@ -1,9 +1,17 @@
 (() => {
     const userInfo = navigator.userAgent.toLowerCase();
     if(userInfo.indexOf('android') > -1) {
-        window.location.href = "https://play.google.com/store/apps/details?id=net.daum.android.map"
+        setTimeout(() => {
+            window.location.href = "https://play.google.com/store/apps/details?id=com.yourssu.ground&hl=ko&gl=US"
+        }, 5000);
+        window.location.href = "https://play.google.com/store/apps/details?id=com.yourssu.ground&hl=ko&gl=US"
+        // 인텐트 주소
     } else if(userInfo.indexOf('iphone') > -1 || userInfo.indexOf('ipad') > -1 || userInfo.indexOf('ipod') > -1) {
-        window.location.href = "https://itunes.apple.com/kr/app/instagram/id389801252?mt=8";
+        setTimeout(() => {
+            window.location.href = "https://apps.apple.com/kr/app/%EA%B7%B8%EB%9D%BC%EC%9A%B4%EB%93%9C/id1483838254"
+        }, 5000);
+        window.location.href = "https://apps.apple.com/kr/app/%EA%B7%B8%EB%9D%BC%EC%9A%B4%EB%93%9C/id1483838254";
+        
     } else {
         console.log('아무것도 아님')
     }
