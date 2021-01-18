8962cc0f93808bfb58869b0276df3c2fcaa355c6
unknown <jwisgenius@naver.com>
2021-01-19 01:28:53 +0900

    deeplink

--- deeplink/script.js
+++ deeplink/script.js
@@ -2,16 +2,15 @@
     const userInfo = navigator.userAgent.toLowerCase();
     if(userInfo.indexOf('android') > -1) {
         setTimeout(() => {
-            window.open("https://play.google.com/store/apps/details?id=com.yourssu.ground&hl=ko&gl=US")
+            window.location.href("https://play.google.com/store/apps/details?id=com.yourssu.ground&hl=ko&gl=US")
         }, 5000);
-            // window.open("https://play.google.com/store/apps/details?id=com.yourssu.ground&hl=ko&gl=US")
-            window.location.replace("https://play.google.com/store/apps/details?id=com.yourssu.ground&hl=ko&gl=US")
         // 인텐트 주소
+            window.location.href('intent://instagram.com/#Intent;package=com.instagram.android;scheme=https;end')
     } else if(userInfo.indexOf('iphone') > -1 || userInfo.indexOf('ipad') > -1 || userInfo.indexOf('ipod') > -1) {
         setTimeout(() => {
             window.location.href = "https://itunes.apple.com/kr/app/%EA%B7%B8%EB%9D%BC%EC%9A%B4%EB%93%9C/id1483838254";
         }, 5000);
-        window.location.href = "https://itunes.apple.com/kr/app/%EA%B7%B8%EB%9D%BC%EC%9A%B4%EB%93%9C/id1483838254";
+        window.location.href = 'instagram://media';
     } else {
         console.log('아무것도 아님')
     }
