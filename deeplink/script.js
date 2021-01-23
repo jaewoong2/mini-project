c6007bc696b2c839d4148ec67b3a6de980024661
unknown <jwisgenius@naver.com>
2021-01-23 18:36:50 +0900

    ground-deeplink

--- deeplink/script.js
+++ deeplink/script.js
@@ -8,20 +8,20 @@
     }
 
     const userInfo = navigator.userAgent.toLowerCase();
+    // 안드로이드
     if(userInfo.indexOf('android') > -1) {
-        // setTimeout(() => {
-        //     window.location.href = "https://play.google.com/store/apps/details?id=com.yourssu.ground&hl=ko&gl=US"
-        // }, 5000);
-        // 인텐트 주소
             setTimeout(async () => {
-                window.location.href = 'intent://instagram.com/#Intent;package=com.instagram.android;scheme=https;end';
+                window.location.href = 'ground://posts/';
                 await sleep(2000)
+                // 앱이 없으면
                 window.location.href = "https://play.google.com/store/apps/details?id=com.yourssu.ground&hl=ko&gl=US"
             }, 0)
     } else if(userInfo.indexOf('iphone') > -1 || userInfo.indexOf('ipad') > -1 || userInfo.indexOf('ipod') > -1) {
+        // ios
         setTimeout(async () => {
-            window.location.href = 'instagram://media';
+            window.location.href = 'https://share.ground.com/posts/';
             await sleep(2000)
+            // 앱이 없으면
             window.location.href = "https://itunes.apple.com/kr/app/%EA%B7%B8%EB%9D%BC%EC%9A%B4%EB%93%9C/id1483838254";
         }, 0);
     } else {
