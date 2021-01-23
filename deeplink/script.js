d0f6cf7fa0bccbc25f49a9ed1bfbce00af0a433f
unknown <jwisgenius@naver.com>
2021-01-23 19:34:45 +0900

    final

--- deeplink/script.js
+++ deeplink/script.js
@@ -11,10 +11,12 @@
     // 안드로이드
     if(userInfo.indexOf('android') > -1) {
             setTimeout(async () => {
-                window.location.href = 'ground://posts';
+                window.location.href = 'ground://';
                 await sleep(2000)
                 // 앱이 없으면
-                window.location.href = "https://play.google.com/store/apps/details?id=com.yourssu.ground&hl=ko&gl=US"
+                if(window.confrim('플레이 스토어로 가시겠습니까?')) {
+                    window.location.href = "https://play.google.com/store/apps/details?id=com.yourssu.ground&hl=ko&gl=US"
+                }
             }, 0)
     } else if(userInfo.indexOf('iphone') > -1 || userInfo.indexOf('ipad') > -1 || userInfo.indexOf('ipod') > -1) {
         // ios
@@ -22,7 +24,10 @@
             window.location.href = 'https://share.yourssu.com/posts/1/1';
             await sleep(2000)
             // 앱이 없으면
-            window.location.href = "https://itunes.apple.com/kr/app/%EA%B7%B8%EB%9D%BC%EC%9A%B4%EB%93%9C/id1483838254";
+
+            if(window.confrim('앱 스토어로 가시겠습니까?')) {
+                window.location.href = "https://itunes.apple.com/kr/app/%EA%B7%B8%EB%9D%BC%EC%9A%B4%EB%93%9C/id1483838254";
+            }
         }, 0);
     } else {
     }
