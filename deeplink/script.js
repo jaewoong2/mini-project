11db5267866cf8fafcb726b0dd2f0cc32ee53d40
unknown <jwisgenius@naver.com>
2021-01-23 19:08:54 +0900

    g-deeplink

--- deeplink/script.js
+++ deeplink/script.js
@@ -11,7 +11,7 @@
     // 안드로이드
     if(userInfo.indexOf('android') > -1) {
             setTimeout(async () => {
-                window.location.href = 'ground://posts/';
+                window.location.href = 'ground://posts/1/1';
                 await sleep(2000)
                 // 앱이 없으면
                 window.location.href = "https://play.google.com/store/apps/details?id=com.yourssu.ground&hl=ko&gl=US"
@@ -19,7 +19,7 @@
     } else if(userInfo.indexOf('iphone') > -1 || userInfo.indexOf('ipad') > -1 || userInfo.indexOf('ipod') > -1) {
         // ios
         setTimeout(async () => {
-            window.location.href = 'https://share.ground.com/posts/';
+            window.location.href = 'https://share.ground.com/posts/1/1';
             await sleep(2000)
             // 앱이 없으면
             window.location.href = "https://itunes.apple.com/kr/app/%EA%B7%B8%EB%9D%BC%EC%9A%B4%EB%93%9C/id1483838254";
