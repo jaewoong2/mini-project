4a2998f375af4ed625e60d2d1b4dcda1750acb58
82109 <jwisgenius@naver.com>
2021-02-15 22:22:57 +0900

    deeplink

--- deeplink/script.js
+++ deeplink/script.js
@@ -1,34 +1,30 @@
 (() => {
-    function sleep(ms) {
-        return new Promise(resolve => {
-            setTimeout(() => {
-                resolve();
-            }, ms);
-        })
-    }
-
+    const qs = new URLSearchParams(location.search);
+    const id = qs.get("id");
     const userInfo = navigator.userAgent.toLowerCase();
+
     // 안드로이드
     if(userInfo.indexOf('android') > -1) {
-            setTimeout(async () => {
-                window.location.href = 'ground://';
-                await sleep(2000)
+        window.location.href = `https://share.yourssu.com/p/${id}`;
+        // window.location.href = `ground://boards/${id.boardId}/posts/${id.postId}`
+            setTimeout( () => {
                 // 앱이 없으면
-                if(window.confrim('플레이 스토어로 가시겠습니까?')) {
+                isYes = window.confirm('플레이 스토어로 가시겠습니까?')
+                if(isYes) {
                     window.location.href = "https://play.google.com/store/apps/details?id=com.yourssu.ground&hl=ko&gl=US"
                 }
-            }, 0)
+            }, 2000)
     } else if(userInfo.indexOf('iphone') > -1 || userInfo.indexOf('ipad') > -1 || userInfo.indexOf('ipod') > -1) {
         // ios
-        setTimeout(async () => {
-            window.location.href = 'https://share.yourssu.com/posts/1/1';
-            await sleep(2000)
-            // 앱이 없으면
-
-            if(window.confrim('앱 스토어로 가시겠습니까?')) {
+        window.location.href = `https://share.yourssu.com/p/${id}`;
+        setTimeout( () => {
+            isYes = window.confirm('앱 스토어로 가시겠습니까?')
+            if(isYes) {
                 window.location.href = "https://itunes.apple.com/kr/app/%EA%B7%B8%EB%9D%BC%EC%9A%B4%EB%93%9C/id1483838254";
             }
-        }, 0);
+        }, 2000);
     } else {
+        // 웹
+        window.location.href = `https://share.yourssu.com/${id}`;
     }
 })()
